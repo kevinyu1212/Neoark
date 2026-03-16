4af33cd181de119fa0059ba29e7513c8068576b5
유건우 <kevinyu1212@naver.com>
2026-03-16 10:03:37 +0900

    feat: initialize spring boot project without lombok

---
+++ neoark-backend/src/main/java/com/neoark/neoark_backend/NeoarkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.neoark.neoark_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoarkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoarkBackendApplication.class, args);
+	}
+
+}
---
+++ neoark-backend/src/test/java/com/neoark/neoark_backend/NeoarkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoark.neoark_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoarkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
